90a779fed3be8855bb49c40f5bb1586259fe2960
roksva123 <ikilua43@gmail.com>
2026-09-20 15:28:48 +0700

    asfdasg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-webresto/', // wajib ada nama repositori diawali dan diakhiri slash /
 })
