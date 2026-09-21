734a33b52200f10ffe3958d1f6bc39f8634b456e
roksva123 <ikilua43@gmail.com>
2026-09-21 13:10:55 +0700

    safdafadsgds

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-webresto/', // wajib ada nama repositori diawali dan diakhiri slash /
+  base: '/', // <-- Pastikan bernilai '/' untuk Vercel
 })
